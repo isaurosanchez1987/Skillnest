902125e404fc829592ad8591971b43ec47ba9f50
andresdino <andresdino9ster@gmail.com>
2025-04-02 13:35:25 -0500

    Cargando proyecto Inicial y configuracion del Pom

---
+++ mired-app/src/main/java/com/mired/mired_app/MiredAppApplication.java
@@ -0,0 +1,13 @@
+package com.mired.mired_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiredAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiredAppApplication.class, args);
+	}
+
+}
---
+++ mired-app/src/test/java/com/mired/mired_app/MiredAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mired.mired_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiredAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
