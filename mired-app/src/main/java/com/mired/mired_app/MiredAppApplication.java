885801d2d5173096d260d59a86f0f6db95aba4f5
isaurosanchez1987 <isaurosancheztrader@gmail.com>
2025-04-04 13:21:05 -0500

    Ajustes de configuracion

--- mired-app/src/main/java/com/mired/mired_app/MiredAppApplication.java
+++ mired-app/src/main/java/com/mired/mired_app/MiredAppApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -15,6 +19,22 @@ public class MiredAppApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(MiredAppApplication.class);
+	}
+
+	@Configuration
+	public static class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 }
 
